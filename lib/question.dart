436671ea087380857809a1d7938dd828ad91b094
Murilo Silva <murilo.alex.silva@gmail.com>
2020-11-04 09:41:10 -0300

    criando as classes

---
+++ lib/question.dart
@@ -0,0 +1,10 @@
+class Question {
+  String questionText;
+  bool questionAnswer;
+
+  //Abaixo seria o construtor da classe
+  Question({String q, bool a}) {
+    questionText = q;
+    questionAnswer = a;
+  }
+}
